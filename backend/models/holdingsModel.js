9a04667eabc7ee291e99b085829593195a819176
nandikaanusha-11 <nandikaanusha11@gmail.com>
2026-06-22 10:43:05 +0530

    connected to database

---
+++ backend/models/holdingsModel.js
@@ -0,0 +1,7 @@
+//require model from mongoose package
+const {model} =require('mongoose');
+const {holdingsSchema}=require("./schemas/holdingsSchema.js");
+
+//"holding", mongodb collection will automatically change it to holdings
+const holdingsModel=new model("holding",holdingsSchema);
+module.exports={holdingsModel};
---
+++ backend/schemas/holdingsSchema.js
@@ -0,0 +1,13 @@
+//require schema from mongoose package
+const {Schema} =require('mongoose');
+
+const holdingsSchema=new Schema({
+  name:String,
+  qty:Number,
+  avg:Number,
+  price:Number,
+  net:String,
+  day:String,
+});
+
+module.exports={holdingsSchema};
