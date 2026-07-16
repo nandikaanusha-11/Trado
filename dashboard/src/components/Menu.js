e0ccc89561984550c8f4a0a01afef2b165c7e5cf
nandikaanusha-11 <nandikaanusha11@gmail.com>
2026-07-16 21:54:49 +0530

    Fix React Router links

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -30,31 +30,31 @@ const Menu = () => {
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="https://trado-1-j98g.onrender.com/orders" 
+            <Link style={{textDecoration:"none"}} to="/orders" 
             onClick={()=>handleMenuClick(1)}>
               <p className={selectedMenu===1 ? activeMenuClass:menuClass}>Orders</p>
             </Link>
           </li>
           <li>
-             <Link style={{textDecoration:"none"}} to="https://trado-1-j98g.onrender.com/holdings" 
+             <Link style={{textDecoration:"none"}} to="/holdings" 
             onClick={()=>handleMenuClick(2)}>
               <p className={selectedMenu===2 ? activeMenuClass:menuClass}>Holdings</p>
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="https://trado-1-j98g.onrender.com/positions" 
+            <Link style={{textDecoration:"none"}} to="/positions" 
             onClick={()=>handleMenuClick(3)}>
               <p className={selectedMenu===3 ? activeMenuClass:menuClass}>Positions</p>
             </Link>
           </li>
           <li>
-             <Link style={{textDecoration:"none"}} to="https://trado-1-j98g.onrender.com/funds" 
+             <Link style={{textDecoration:"none"}} to="funds" 
             onClick={()=>handleMenuClick(4)}>
               <p className={selectedMenu===4? activeMenuClass:menuClass}>Fundings</p>
             </Link>
           </li>
           <li>
-             <Link style={{textDecoration:"none"}} to="https://trado-1-j98g.onrender.com/apps" 
+             <Link style={{textDecoration:"none"}} to="/apps" 
             onClick={()=>handleMenuClick(5)}>
               <p className={selectedMenu===5 ? activeMenuClass:menuClass}>Apps</p>
             </Link>
