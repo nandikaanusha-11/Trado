6c75afcbc8aceca35bbfe2733b27af53035341cf
nandikaanusha-11 <nandikaanusha11@gmail.com>
2026-07-19 09:15:26 +0530

    Frontend URL was added to CORS origin

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,8 @@ const { MONGO_URL} = process.env;
 app.use(
   cors({
     origin: ["http://localhost:3000",
-"https://trado-3.onrender.com",],
+"https://trado-3.onrender.com",
+"https://trado-4.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
